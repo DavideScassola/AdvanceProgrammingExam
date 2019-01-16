f7c1042092b7a04a3e27574e78c11ca9e5964077
Davide Scassola <projanko@gmail.com>
2019-01-16 10:17:30 +0100

    fixed little things on performance tester

--- c++/Performance_test.cpp
+++ c++/Performance_test.cpp
@@ -13,43 +13,36 @@ int main(int arcv, char *argv[])
 	std::srand(0);
 	const size_t N = (arcv<2) ? 10000 : atoi(argv[1]);
 	const bool full_test = (arcv<3) ? true : false;
-	const size_t load_size = 2; //how many byte for each data
+	//const size_t load_size = 2; //how many byte for each data
 
-	BinaryTree<const int, int> linked_list_tree;
-	BinaryTree<const int, int> balanced_tree;
-	BinaryTree<const int, int> random_tree;
-	std::map<const int, int> map;
-	
-	
-	//linked_list_tree.insert(12,int{int_size});
+	BinaryTree<const int, double> linked_list_tree;
+	BinaryTree<const int, double> balanced_tree;
+	BinaryTree<const int, double> random_tree;
+	std::map<const int, double> map;
 
 	
 	//////////// Initializing trees ////////////
 	std::cout << "size = "<< N << std::endl;
 	
 
-	if(full_test){
-	std::cout << "initializing linked_list_tree . . ." << std::endl;
-	for(int i = 0; i<N; i++)
+	if(full_test)
 	{
-		linked_list_tree.insert(i,0);
-	}
+		std::cout << "initializing linked_list_tree . . ." << std::endl;
+		for(int i = 0; i<N; i++)
+		{
+			linked_list_tree.insert(i,i);
+		}
 	}
 
 	std::cout << "initializing std::map  . . ." << std::endl;
 	for(int i = 0; i<N; i++)
 	{
-		map.insert(std::make_pair(i,0));
+		map.insert(std::make_pair(i,i));
 	}
-
-	
-
-	
-
 
 
 	// building a vector of random ordered numbers
-	std::vector<int> random_permutation_vector;
+	std::vector<double> random_permutation_vector;
 	for(int i = 0; i<N; i++)
 		random_permutation_vector.push_back(i);
 	std::random_shuffle ( random_permutation_vector.begin(), random_permutation_vector.end() );
@@ -59,17 +52,15 @@ int main(int arcv, char *argv[])
 	std::cout << "initializing random tree . . ." << std::endl;
 	for(auto e : random_permutation_vector)
 	{
-		random_tree.insert(e,0);
+		random_tree.insert(e,e);
 	}
-
-	balanced_tree = random_tree;
 
 	// balancing one of the trees
 	std::cout << "balancing tree . . ." << std::endl;
+	balanced_tree = random_tree;
 	balanced_tree.balance();
 
 	
-
 	//////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -80,23 +71,26 @@ int main(int arcv, char *argv[])
 	// reshuffling for random access
 	std::random_shuffle ( random_permutation_vector.begin(), random_permutation_vector.begin());
 
-	std::cout << "accessing all the elements . . ." << std::endl;
-
-	//just for initializing them
+	// just for initializing them
 	auto begin = std::chrono::high_resolution_clock::now();
 	auto end = std::chrono::high_resolution_clock::now();
 	auto total = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
-	
+
+
+
+	std::cout << "accessing all the elements . . ." << std::endl;
+
 	//LINKED LIST TREE
-	if(full_test) {
-	begin = std::chrono::high_resolution_clock::now();
-	for(auto e : random_permutation_vector)
+	if(full_test)
 	{
-		linked_list_tree.find(e);
-	}
-	end = std::chrono::high_resolution_clock::now();
-	total = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
-	std::cout << "LINKED_LIST_TREE: " << total << "us, average = " << total/N << "us" << std::endl;
+		begin = std::chrono::high_resolution_clock::now();
+		for(auto e : random_permutation_vector)
+		{
+			linked_list_tree.find(e);
+		}
+		end = std::chrono::high_resolution_clock::now();
+		total = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
+		std::cout << "LINKED_LIST_TREE: " << total << "us, average = " << total/N << "us" << std::endl;
 	}
 
 	//RANDOM TREE
@@ -119,7 +113,7 @@ int main(int arcv, char *argv[])
 	total = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
 	std::cout << "BALANCED_TREE: " << total << "us, average = " << total/double(N) << "us" << std::endl;
 
-	//MAP PERFORMANCES
+	//MAP
 	begin = std::chrono::high_resolution_clock::now();
 	for(auto e : random_permutation_vector)
 	{
@@ -128,8 +122,6 @@ int main(int arcv, char *argv[])
 	end = std::chrono::high_resolution_clock::now();
 	total = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
 	std::cout << "MAP: " << total << "us, average = " << total/double(N) << "us" << std::endl;
-
-	linked_list_tree.clear();
 	
     return 0;
 }
