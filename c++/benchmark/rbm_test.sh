cb28947cb9f9ddcaff97246f08c234ebc671278b
Davide Scassola <projanko@gmail.com>
2019-01-18 11:46:30 +0100

    reorganized files in folders

--- c++/benchmark/rbm_test.sh
+++ c++/benchmark/rbm_test.sh
@@ -1,13 +1,13 @@
-#ITER=1
-
 N_SEQ="1 2 4 6 8 10 12 14 16 18 20"
 BNAME=bench.x
+FIRST=1
+LAST=10
 
-#for i in $(seq $FIRST_MAT_SIZE $LAST_MAT_SIZE); do
 echo "N;RANDOM;BALANCED;STD_MAP"
-for i in $N_SEQ; do       
-        N=$((i*1000000))
-	./$BNAME $N ciao
+for i in $(seq $FIRST $LAST); do
+	for i in $N_SEQ; do       
+        	N=$((i*1000000))
+		./$BNAME $N ciao
+	done
 done
-
 exit
